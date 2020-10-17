2cc154108ac833c693396edc40b2d882c41b63b3
Bethsheba Zebata <bethzebata@gmail.com>
2020-10-17 16:56:48 -0500

    added a function for time over indy but still cant figure out how to display the text

--- iss.py
+++ iss.py
@@ -2,26 +2,37 @@
 
 __author__ = 'Bethsheba Zebata'
 
-import requests
+
 import turtle
-import urllib
+import urllib.request
 import json
 import time
 
 
+def indy():
+    url = 'http://api.open-notify.org/iss-pass.json?lat=39.7&lon=86.1&alt=20&n=5'
+    response = urllib.request.urlopen(url)
+    result = json.loads(response.read())
+    time_passed = result["response"]
+    for t in time_passed:
+        datetime = time.ctime(t['risetime'])
+        print("ISS will be over Indy on  " + datetime)
+
+
 def main():
+    indy()
     url = 'http://api.open-notify.org/astros.json'
     response = urllib.request.urlopen(url)
-    r = json.loads(response.read())
-    print("Total # of astronauts in space: " + str(r["number"]) + "")
+    result = json.loads(response.read())
+    print("Total # of astronauts in space: " + str(result["number"]) + "")
 
-    people = r["people"]
+    people = result["people"]
 
     for p in people:
         print(p["name"] + " is aboard " + p["craft"])
 
     s = turtle.Screen()
-    s.setup(800, 800)
+    s.setup(720, 360)
     s.setworldcoordinates(-180, -90, 180, 90)
     s.bgpic('map.gif')
     s.register_shape("iss.gif")
@@ -37,13 +48,14 @@ def main():
     while True:
         url = 'http://api.open-notify.org/iss-now.json'
         response = urllib.request.urlopen(url)
-        r = json.loads(response.read())
-        location = r["iss_position"]
+        result = json.loads(response.read())
+        location = result["iss_position"]
         lat = location["latitude"]
         lon = location["longitude"]
 
         print("Latitude: " + str(lat))
         print("Longitude: " + str(lon))
+        print("time: " + str(time.ctime(result['timestamp'])))
 
         t.goto(float(lon), float(lat))
         time.sleep(5)
